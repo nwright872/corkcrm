cd1275f07168a3f99d14a3ca4c9e47df4302ba26
Nick Wright <nwright872@gmail.com>
2016-08-09 09:45:19 -0400

    Created initial CorkCRM skeleton

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var nunjucksRender = require('gulp-nunjucks-render');
+
+
+gulp.task('nunjucks', function() {
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('app/pages/**/*.+(html|nunjucks)')
+  // Renders template with nunjucks
+  .pipe(nunjucksRender({
+      path: ['app/templates']
+    }))
+  // output files in app folder
+  .pipe(gulp.dest('app'))
+});
